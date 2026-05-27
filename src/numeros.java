d72dea22d072f5e691f2fa8935217ce526061880
Carlos Quiñonez <carlosvalorantve1@gmail.com>
2026-05-27 08:30:39 -0600

    FEAT: Finalizado el programa para poder ejecutarlo

--- src/numeros.java
+++ src/numeros.java
@@ -1,3 +1,17 @@
 public class numeros {
+    public static void main(String[] args) {
+        int[] lista = {1, 2, 9, 8, 5, 7, 6, 4, 6};
 
+        int mayor = lista[0];
+        int posicion = 0;
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] > mayor) {
+                mayor = lista[i];
+                posicion = i;
+            }
+        }
+        System.out.println("la posicion del numero  es:" + posicion);
+        System.out.println("el numero mayor es:" + mayor);
+
+    }
 }
